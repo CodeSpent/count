69708880a1f67458172d9a383dd6892b99e8cb86
Sacra Volskaya <sacravolskaya@protonmail.ch>
2018-07-16 07:22:11 +0300

    Lexical bounds

--- main.js
+++ main.js
@@ -15,113 +15,113 @@
 const Discord = require("discord.js");
 const Config = require("./config.json");
 
-let server = null;  // Assigned, after bot is ready and correct server ID found
-let updates = 0;  // Count dispatched updates
-
-const state = {
-  members: 0,
-  users: 0,
-  bots: 0,
-};
-
-const channels = {
-  totalCount: {
-    id: Config.totalChannel.id,
-    ref: null,
-    state: 'members',
-    name: Config.totalChannel.name
-  },
-  onlineCount: {
-    id: Config.onlineChannel.id,
-    ref: null,
-    state: 'users',
-    name: Config.onlineChannel.name
-  },
-  botCount: {
-    id: Config.botChannel.id,
-    ref: null,
-    state: 'bots',
-    name: Config.botChannel.name
-  }
-};
-
-const formatMetrics = () => `Total members - ${state.members}, `
-      + `Online users - ${state.users}, `
-      + `Bots - ${state.bots}`;
-
-// Count both of them, to avoid iterating too many times
-const getMetrics = () => {
-  if (server == null) [0, 0, 0];
-
-  let members = server.members.size,
-      users = 0,
-      bots = 0;
-
-  server.members.map(m => {
-    if (m.user.bot) bots++;
-    else if (m.presence.status == 'online') users++;
-  });
-
-  return [members, users, bots];
-};
-
-// Recalculate metrics
-const invalidateState = () => {
-  const [members, users, bots] = getMetrics();
-
-  state.members = members;
-  state.users = users;
-  state.bots = bots;
-};
-
-// Iterate trough available servers and update their counters
-// It also recalculates metrics within the same loop
-const updateCounters = async () => {
-  invalidateState();  // Update metrics, before setting channel names
-
-  updates++;
-
-  console.log(`[${updates}] Updating counters…`);
-  console.log(formatMetrics());
-
-  return Promise.all(
-    Object.values(channels).map(async channel => {
-      if (channel.ref != null) {
-        await channel.ref.setName(`${channel.name} : ${state[channel.state]}`)
-               .catch(err => {
-                 if (err.code == 50001)
-                   console.log('Bot does not have enough permissions '
-                               + `to modify #${channel.ref.name}`);
-               });
-      } else {
-        console.log(`Tried to update ${channel.name} count, `
-                    + 'but the reference to the channel is missing');
-      }
-    }));
-};
-
-// Will print every channel and its ID within the Config.server
-// Use the output to get the references for counter channels
-const assignReferences = () => {
-  let i = 0;
-
-  console.log(`Connected to ${server.name}, available channels:`);
-
-  server.channels.forEach(channel => {
-    console.log(`    [${i}] ${channel.name}, id - ${channel.id}`);
-
-    Object.keys(channels).forEach(key => {
-      if (channel.id == channels[key].id) {
-        console.log(`Reference found for ${channels[key].name}`);
-        channels[key].ref = channel;
-      }
-    })
-  });
-
-  console.log('');  // New line
-}
-
 (function() {  // Encapsulate bot, just in case
+  let server = null;  // Assigned, after bot is ready and correct server ID found
+  let updates = 0;  // Count dispatched updates
+
+  const state = {
+    members: 0,
+    users: 0,
+    bots: 0,
+  };
+
+  const channels = {
+    totalCount: {
+      id: Config.totalChannel.id,
+      ref: null,
+      state: 'members',
+      name: Config.totalChannel.name
+    },
+    onlineCount: {
+      id: Config.onlineChannel.id,
+      ref: null,
+      state: 'users',
+      name: Config.onlineChannel.name
+    },
+    botCount: {
+      id: Config.botChannel.id,
+      ref: null,
+      state: 'bots',
+      name: Config.botChannel.name
+    }
+  };
+
+  const formatMetrics = () => `Total members - ${state.members}, `
+        + `Online users - ${state.users}, `
+        + `Bots - ${state.bots}`;
+
+  // Count both of them, to avoid iterating too many times
+  const getMetrics = () => {
+    if (server == null) [0, 0, 0];
+
+    let members = server.members.size,
+        users = 0,
+        bots = 0;
+
+    server.members.map(m => {
+      if (m.user.bot) bots++;
+      else if (m.presence.status == 'online') users++;
+    });
+
+    return [members, users, bots];
+  };
+
+  // Recalculate metrics
+  const invalidateState = () => {
+    const [members, users, bots] = getMetrics();
+
+    state.members = members;
+    state.users = users;
+    state.bots = bots;
+  };
+
+  // Iterate trough available servers and update their counters
+  // It also recalculates metrics within the same loop
+  const updateCounters = async () => {
+    invalidateState();  // Update metrics, before setting channel names
+
+    updates++;
+
+    console.log(`[${updates}] Updating counters…`);
+    console.log(formatMetrics());
+
+    return Promise.all(
+      Object.values(channels).map(async channel => {
+        if (channel.ref != null) {
+          await channel.ref.setName(`${channel.name} : ${state[channel.state]}`)
+                       .catch(err => {
+                         if (err.code == 50001)
+                           console.log('Bot does not have enough permissions '
+                                       + `to modify #${channel.ref.name}`);
+                       });
+        } else {
+          console.log(`Tried to update ${channel.name} count, `
+                      + 'but the reference to the channel is missing');
+        }
+      }));
+  };
+
+  // Will print every channel and its ID within the Config.server
+  // Use the output to get the references for counter channels
+  const assignReferences = () => {
+    let i = 0;
+
+    console.log(`Connected to ${server.name}, available channels:`);
+
+    server.channels.forEach(channel => {
+      console.log(`    [${i}] ${channel.name}, id - ${channel.id}`);
+
+      Object.keys(channels).forEach(key => {
+        if (channel.id == channels[key].id) {
+          console.log(`Reference found for ${channels[key].name}`);
+          channels[key].ref = channel;
+        }
+      })
+    });
+
+    console.log('');  // New line
+  }
+
   if (Config.token == "") {
     console.error("Please add a valid token to config.json");
     process.exit();
